81ed5fbb2bf2295afbb501b96aee3121d5a79db2
Dark-Ninja1 <sudarsanraut1@gmail.com>
2021-09-25 03:12:28 +0530

    NewsApp Added

---
+++ app/src/main/java/com/sudarsan/newsapp2/MainActivity.java
@@ -0,0 +1,71 @@
+package com.sudarsan.newsapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    TabItem mhome,msport,mscience,mhealth,mtech,menterview;
+    PageAdapter pageAdapter;
+    Toolbar mtoobar;
+
+    String api="ab0d74b21eaa4faf958dd45a927efd0e";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+//        mtoobar = findViewById(R.id.toolbar);
+//        setSupportActionBar(mtoobar);
+
+        mhome = findViewById(R.id.home);
+        msport = findViewById(R.id.sports);
+        mscience = findViewById(R.id.science);
+        mhealth = findViewById(R.id.health);
+        mtech = findViewById(R.id.technology);
+        menterview = findViewById(R.id.enterainment);
+
+
+        ViewPager viewPager = findViewById(R.id.fragmentcontainer);
+        tabLayout= findViewById(R.id.include);
+
+        pageAdapter = new PageAdapter(getSupportFragmentManager(),6);
+        viewPager.setAdapter(pageAdapter);
+
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if (tab.getPosition()==0||tab.getPosition()==1||tab.getPosition()==2||tab.getPosition()==3||tab.getPosition()==4||tab.getPosition()==5){
+                    pageAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/sudarsan/newsapp2/PageAdapter.java
@@ -0,0 +1,51 @@
+package com.sudarsan.newsapp2;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class PageAdapter extends FragmentPagerAdapter {
+
+    int tabcount;
+
+    public PageAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+        tabcount = behavior;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position)
+        {
+            case 0:
+                return new HomeFragment();
+
+            case 1:
+                return new SportsFragment();
+
+            case 2:
+                return new HeathFragment();
+
+            case 3:
+                return new ScienceFragment();
+
+            case 4:
+                return new EnventeroFragment();
+
+            case 5:
+                return new TechnologyFragment();
+
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabcount;
+    }
+}
